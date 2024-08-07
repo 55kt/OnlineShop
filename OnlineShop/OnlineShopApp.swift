35bff0c1698432de129d5adece3e755d4d21aad4
Vlad <160039489+55kt@users.noreply.github.com>
2024-08-07 19:02:52 +0300

    first commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Vlad on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
